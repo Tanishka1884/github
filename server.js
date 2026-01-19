4c96b78a5e62db012c2981ed63ff87a166171294
tanishka <tanishkagwaliory@gmail.com>
2026-01-20 01:27:55 +0530

    added more js

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
-// Requiring the core HTTP module
-const http = require('http'); [1]
+const http = require('http'); // Implied by server creation [3]
+const fs = require('fs'); // Required for reading HTML files [1]
 
-// Creating the server
-const server = http.createServer((req, res) => { [1]
-    // This callback runs every time a request is made to the server
-    console.log('request made'); [2]
-}); [1]
+const server = http.createServer((req, res) => {
+  // 1. Set header content type [1, 4]
+  res.setHeader('Content-Type', 'text/html');
 
-// Setting the server to listen for requests
-server.listen(3000, 'localhost', () => { [2]
-    // This callback fires when the server starts listening
-    console.log('listening for requests on port 3000'); [2]
-}); [2]
+  // 2. Simple Routing Logic [5-7]
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200; // Success status [8, 9]
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200; // Success status [9]
+      break;
+    case '/about-me': // Redirect logic [10, 11]
+      res.statusCode = 301; // Moved permanently [11]
+      res.setHeader('Location', '/about'); // Redirect to new URL [12]
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404; // Not found status [9]
+      break;
+  }
+
+  // 3. Read the HTML file and send response [1, 7, 13, 14]
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end(); // End response on error to avoid hanging [13]
+    } else {
+      // res.write(data); // Option to write data separately [13]
+      res.end(data); // Sending data directly through end() [14]
+    }
+  });
+});
+
+// 4. Start the server on port 3000 [3]
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
